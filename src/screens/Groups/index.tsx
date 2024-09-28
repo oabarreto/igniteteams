b9c7abfc69e4511c3c4b5662cf6a7d742762c2b7
Otávio Barreto <oabarreto@ichurch.com.br>
2024-09-28 17:19:24 -0300

    "Added Highlight component to Groups screen"

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title={"Galera"} subtitle={"Reuna sua galera"} />
     </Container>
   );
 }
