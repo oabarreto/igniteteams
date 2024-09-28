aca9bcdf5aceec5e94ccc7861da929e115242379
Otávio Barreto <oabarreto@ichurch.com.br>
2024-09-28 15:48:11 -0300

    "Added Roboto font, updated dependencies, and refactored code in App.tsx, babel.config.js, package-lock.json, package.json, and src/screens/Groups/index.tsx"

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
+import theme from "src/theme";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
