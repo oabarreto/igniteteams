5bff026a744aad45a746eddd6b0021f8e575c56f
Otávio Barreto <oabarreto@ichurch.com.br>
2024-09-28 17:56:23 -0300

    "Added useState and FlatList to Groups screen, replaced static GroupCard with dynamic list"

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Minha Galera",
+    "Amigos",
+    "Trabalho",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title={"Galera"} subtitle={"Reuna sua galera"} />
 
-      <GroupCard title={"Galera do trabalho"} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
